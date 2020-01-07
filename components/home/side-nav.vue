8f93b8b42b4cdf26eaa3d2a37078b88e3616c9b1
MINT <mint@insdim.com>
2020-01-07 11:59:22 +0800

    remove timeline

--- components/home/side-nav.vue
+++ components/home/side-nav.vue
@@ -63,10 +63,10 @@ export default class HomeSideNav extends Vue {
         path: 'collection',
         title: 'links.collection'
       },
-      {
-        path: 'timeline',
-        title: 'links.timeline'
-      },
+      // {
+      //   path: 'timeline',
+      //   title: 'links.timeline'
+      // },
       {
         path: 'about',
         title: 'links.about'
